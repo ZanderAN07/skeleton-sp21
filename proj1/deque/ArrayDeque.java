77a1a6bfaf3a9c0b828e9627c21fee2befa166d5
ZanderAN07 <zanderan615@gmail.com>
2025-01-23 09:41:27 +0800

    update

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -4,33 +4,43 @@ public class ArrayDeque<T> {
     private T[] items;
     private int size;
     private int first; //（数组的第一个数字的index）
-    public ArrayDeque(){
+
+    public ArrayDeque() {
         items = (T[]) new Object[8];
         first = 3;
         size = 0;
     }
-    private void resize(int capacity){
+
+    private void resize(int capacity) {
         T[] a = (T[]) new Object[capacity];
-        System.arraycopy(items, first, a, first+capacity/2 , size);
-        first += capacity/2;
+        System.arraycopy(items, first, a, first + capacity / 2, size);
+        first += capacity / 2;
         items = a;
     }
-    public void addLast(T adder){
-        if(size == items.length){
+
+    public void addLast(T adder) {
+        if (size == items.length) {
             resize(items.length * 2);
         }
-        items[size] = adder;
+        items[size + first] = adder;
         size += 1;
     }
-    public void addFirst(T adder){
-        if(first == 0){
-            resize(items.length *2);
+
+    public void addFirst(T adder) {
+        if (first == 0) {
+            resize(items.length * 2);
+            items[first] = adder;
+        }
+
+        if(items[first] != null){
+            first -= 1;
         }
         items[first] = adder;
-        first -= 1;
+        size += 1;
     }
-    public T removeLast(){
-        if(isEmpty()){
+
+    public T removeLast() {
+        if (isEmpty()) {
             return null;
         }
         T r = items[size - 1];
@@ -38,8 +48,9 @@ public class ArrayDeque<T> {
         size -= 1;
         return r;
     }
-    public T removeFirst(){
-        if(isEmpty()){
+
+    public T removeFirst() {
+        if (isEmpty()) {
             return null;
         }
         T r = items[first];
@@ -48,23 +59,33 @@ public class ArrayDeque<T> {
         size -= 1;
         return r;
     }
-    public int size(){
+
+    public int size() {
         return size;
     }
-    public T get(int index){
+
+    public T get(int index) {
         if (index < 0 || index >= size) {
             return null;
         }
-        return items[first+ index];
+        return items[first + index];
     }
-    public void printDeque(){
-        for(int i = first; i < size; i ++){
+
+    public void printDeque() {
+        for (int i = first; i < size; i++) {
             System.out.print(items[i]);
         }
         System.out.println();
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return size == 0;
     }
 
+    public static void main(String[] args) {
+        ArrayDeque<Integer> test = new ArrayDeque<>();
+        test.addLast(10);
+        test.addFirst(11);
+        System.out.println(test.get(0));
+    }
 }
