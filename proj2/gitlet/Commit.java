00c99882a2d9d74905bfc2ae0f3896903575f81e
ZanderAN07 <zanderan615@gmail.com>
2026-08-18 23:55:38 +0800

    proj2 redo

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,146 +2,25 @@ package gitlet;
 
 // TODO: any imports you need here
 
-import javax.swing.tree.TreeNode;
-import java.io.File;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date; // TODO: You'll likely use this in this class
-import java.util.List;
-
-import static gitlet.Repository.*;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author ZanderAn
+ *  @author TODO
  */
-public class Commit implements Serializable {
+public class Commit {
     /**
      * TODO: add instance variables here.
-     * <p>
+     *
      * List all instance variables of the Commit class here with a useful
      * comment above them describing what that variable represents and how that
      * variable is used. We've provided one example for `message`.
      */
-    private String parent;
 
-    private String parentForMerge;
-
-    /**
-     * The message of this Commit.
-     */
+    /** The message of this Commit. */
     private String message;
 
-    private Date timestamp;
-
-    private TreeNode SnapShot;
-
     /* TODO: fill in the rest of this class. */
-
-    public Commit() {
-        message = "initial commit";
-        parent = null;
-        timestamp = new Date(0);
-        SnapShot = TreeBuilderHelper(CWD);
-    }
-
-    public Commit(String m, Commit p) {
-        message = m;
-        parent = Utils.sha1(p);
-        //复制parent的Snap Shot
-        SnapShot = TreeBuilderHelper(CWD);
-        TreeNode t = p.SnapShot.DeepCopy(p.SnapShot); //太怪了这代码逻辑有点恶心
-        timestamp = new Date();
-        //在commit文件夹里面寻找parent序列化后的文件（关于文件search，是否需要重写方法？）
-    }
-
-    public static String GetParent(Commit c){
-        return c.parent;
-    }
-
-    public static String GetMessage(Commit c){
-        return c.message;
-    }
-
-    public static Date GetTime(Commit c){
-        return c.timestamp;
-    }
-
-    public TreeNode GetSnapShot(){
-        return SnapShot;
-    }   
-
-    public TreeNode TreeBuilderHelper(File f) {//此方法用于递归的创建一个以文件夹关系为导向的tree
-        if (f.isDirectory()) {
-            File[] files = f.listFiles();
-            ArrayList<TreeNode> children = new ArrayList<>();
-
-            if (files != null) {
-                Arrays.sort(files); // 保证遍历顺序一致
-                for (File child : files) {
-                    TreeNode childNode = TreeBuilderHelper(child);
-                    if (childNode != null) {
-                        children.add(childNode);
-                    }
-                }
-            }
-
-            // 注意这里：目录节点没有 hash，只用名字和子节点
-            return new TreeNode(f.getName(), null, children);
-
-        } else {
-            // 是文件时，构造 hash，子节点为 null 或空列表
-            String hash = Utils.sha1(Utils.readContents(f));
-            return new TreeNode(f.getName(), hash, new ArrayList<>());
-        }
-    }
-
-
-    public class TreeNode implements Serializable {
-        private final String name;
-        private final String sha1;
-        private List<TreeNode> ChildList;
-
-        private TreeNode(String n, String s, List<TreeNode> children) {
-            name = n;
-            sha1 = s;
-            ChildList = children;
-        }
-
-        private TreeNode(String n, String s) {
-            name = n;
-            sha1 = s;
-            ChildList = new ArrayList<>();
-        }
-
-        private void addChild(TreeNode f) {
-            ChildList.add(f);
-        }
-
-        public String getName(){
-            return name;
-        }
-
-        public String getSha1(){
-            return sha1;
-        }
-
-        public List<TreeNode> getChildList(){
-            return ChildList;
-        }
-
-        private TreeNode DeepCopy(TreeNode tr) {
-            TreeNode copy = new TreeNode(tr.name, tr.sha1);
-            if (!tr.ChildList.isEmpty()) {
-                for (TreeNode items : tr.ChildList) {
-                    copy.addChild(DeepCopy(items));
-                }
-            }
-            return copy;
-        }
-
-    }
 }
--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,7 +1,5 @@
 package gitlet;
 
-import static gitlet.Repository.FileFinder;
-
 /** Driver class for Gitlet, a subset of the Git version-control system.
  *  @author TODO
  */
@@ -12,53 +10,15 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
-        Repository repo = new Repository();
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
                 // TODO: handle the `init` command
-                repo.init();
                 break;
             case "add":
-                repo.checkInital();
-                //把某一个特定的file加入staging area这个file
-                repo.add(FileFinder(Repository.CWD, args[1]));//TODO：在cwd里面搜索这个文件
+                // TODO: handle the `add [filename]` command
                 break;
             // TODO: FILL THE REST IN
-            case "commit":
-                // 需要检测是否有commit message，throw error
-                repo.checkInital();
-                validateNumArgs("Please enter a commit message.",args, 2);
-                repo.makeNewCommit(args[1]);
-                repo.clearStagingArea();
-                break;
-            case "rm":
-                repo.checkInital();
-                //check num args?
-                repo.remove(FileFinder(Repository.CWD, args[1]));//TODO:Search that file in CWD
-                break;
-            case "branch":
-                repo.checkInital();
-                //check num args?
-                repo.makeNewBranch(args[1]);
-                break;
-
-            case "rm-branch":
-                repo.checkInital();
-                //new method tobe added
-                break;
-
-            case "log":
-                repo.checkInital();
-                break;
-            default:
-                System.out.println("No command with that name exists.");
-            }
-        }
-    public static void validateNumArgs(String errMessage, String[] args, int n) {
-        if (args.length != n) {
-            throw new RuntimeException(errMessage);
         }
     }
 }
-
